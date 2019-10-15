22102e8d610785a9bcdeb79a29a838b440220a35
josealarconchacon <jose.alarcon.chacom@gmail.com>
2019-10-15 18:02:18 -0400

    First commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Jose Alarcon Chacon on 10/15/19.
+//  Copyright © 2019 Jose Alarcon Chacon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
